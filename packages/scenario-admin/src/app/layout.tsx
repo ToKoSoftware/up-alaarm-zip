70b2145fd861644fcdc02d6a539a3f1be6ea63b9
Tomas Kostadinov <github@kostadinov.it>
2023-06-20 00:31:31 +0200

    Fix titles

--- packages/scenario-admin/src/app/layout.tsx
+++ packages/scenario-admin/src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Scenario Client',
+  title: 'Scenario Admin',
   description: '',
 }
 
