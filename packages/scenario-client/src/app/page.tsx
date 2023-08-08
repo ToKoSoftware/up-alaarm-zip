f0e7a7879dbf113e6b5e4e636144f7e6140595b0
Tomas Kostadinov <github@kostadinov.it>
2023-08-09 00:32:37 +0200

    Automatically start QR-Scan

--- packages/scenario-client/src/app/page.tsx
+++ packages/scenario-client/src/app/page.tsx
@@ -2,8 +2,26 @@
 import {Alert, AlertDescription, AlertTitle} from '@/components/ui/alert';
 import * as React from 'react';
 import {Button} from '@/components/ui/button';
+import useSWR from "swr";
+import {ApplicationState, getData} from '@alaarm/shared';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+    const router = useRouter()
+    const fetcher = (url: string) => getData<ApplicationState>('/api/v1/state').then(({data}) => {
+        if (!data) {
+            return data;
+        }
+        console.log(data.scenarioRunning);
+            if (data.scenarioRunning) {
+                router.push('/qr');
+            }
+            return data;
+        }
+    );
+    const {data, error, isLoading} = useSWR('/state', fetcher, {refreshInterval: 1000})
+    ;
+
     return (
         <div className="w-full h-screen flex flex-col items-center justify-center">
             <div className="flex items-center flex-col justify-center space-y-4">
@@ -12,7 +30,8 @@ export default function Home() {
                         Wartebereich
                     </AlertTitle>
                     <AlertDescription>
-                        Bitte warten Sie, bis das Szenario durch einen Mitarbeiter gestartet wird. Dieses Fenster wird automatisch aktualisiert.
+                        Bitte warten Sie, bis das Szenario durch einen Mitarbeiter gestartet wird. Dieses Fenster wird
+                        automatisch aktualisiert.
                     </AlertDescription>
                 </Alert>
 
--- packages/scenario-client/src/app/qr/page.js
+++ packages/scenario-client/src/app/qr/page.js
@@ -1,91 +1,91 @@
 'use client';
 import React, { useState } from 'react';
 import QrReader from 'react-qr-reader-es6';
-import {useRouter} from "next/navigation";
+import {useRouter} from 'next/navigation';
 import {Button} from '@/components/ui/button';
 import './qr.css';
 
 const App = () => {
-  const [scannedCode, setScannedCode] = useState('');
-  const [isError, setIsError] = useState(false);
-  const [isReaderOpen, setIsReaderOpen] = useState(false);
-  const [expectedCode, setExpectedCode] = useState('Machine_02'); // Set initial value to 'Machine_01'
+    const [scannedCode, setScannedCode] = useState('');
+    const [isError, setIsError] = useState(false);
+    const [isReaderOpen, setIsReaderOpen] = useState(false);
+    const [expectedCode, setExpectedCode] = useState('Machine_02'); // Set initial value to 'Machine_01'
 
-  const handleScan = (data) => {
-    if (data) {
-      setScannedCode(data);
-      setIsReaderOpen(false);
+    const handleScan = (data) => {
+        if (data) {
+            setScannedCode(data);
+            setIsReaderOpen(false);
   
-      // Check if the scanned code matches the expected code
-      if(data === expectedCode) {
-        setIsError(false);
-        redirectToGame(data);
-      } else {
-        setIsError(true);
-        // Add a delay of 2000 milliseconds (2 seconds) and then start the QR reader again
+            // Check if the scanned code matches the expected code
+            if(data === expectedCode) {
+                setIsError(false);
+                redirectToGame(data);
+            } else {
+                setIsError(true);
+                // Add a delay of 2000 milliseconds (2 seconds) and then start the QR reader again
+                setTimeout(() => {
+                    setScannedCode('');
+                    setIsReaderOpen(true);
+                }, 2000);
+            }
+        }
+    };
+  
+
+    const handleError = (error) => {
+        console.error(error);
+    };
+
+    const handleButtonClick = () => {
+        setIsReaderOpen(true);
+    };
+
+    const router = useRouter();
+
+    const redirectToGame = (machineCode) => {
         setTimeout(() => {
-          setScannedCode('');
-          setIsReaderOpen(true);
-        }, 2000);
-      }
-    }
-  };
-  
-
-  const handleError = (error) => {
-    console.error(error);
-  };
-
-  const handleButtonClick = () => {
-    setIsReaderOpen(true);
-  };
-
-  const router = useRouter();
-
-  const redirectToGame = (machineCode) => {
-    setTimeout(() => {
-      switch(machineCode) {
-        case 'Machine_01':
-          router.push('quest/numbers-basic');
-          break;
-        case 'Machine_02':
-          router.push('quest/reaction');
-          break;
-        case 'Machine_03':
-          router.push('quest/numbers-advanced');
-          break;
-        default:
+            switch(machineCode) {
+            case 'Machine_01':
+                router.push('quest/numbers-basic');
+                break;
+            case 'Machine_02':
+                router.push('quest/reaction');
+                break;
+            case 'Machine_03':
+                router.push('quest/numbers-advanced');
+                break;
+            default:
           // Handle unexpected machine codes
-      }
-    }, 2000); // Add a delay of 2000 milliseconds (2 seconds)
-  };
+            }
+        }, 2000); // Add a delay of 2000 milliseconds (2 seconds)
+    };
   
-  const renderMessage = () => {
-    if (isError) {
-      return <div className="message">Falscher Code! Gehen Sie zu {expectedCode}.</div>;
-    } else if (scannedCode === expectedCode) {
-      return (
-        <div className="message">
+    const renderMessage = () => {
+        if (isError) {
+            return <div className="message">Falscher Code! Gehen Sie zu {expectedCode}.</div>;
+        } else if (scannedCode === expectedCode) {
+            return (
+                <div className="message">
           Dies ist der richtige Code! Starte Spiel...
-          {/* Add code here to start the game */}
-        </div>
-      );
-    } else {
-      return null;
-    }
-  };
+                    {/* Add code here to start the game */}
+                </div>
+            );
+        } else {
+            return null;
+        }
+    };
 
-  return (
-    <div className="container">
-      {!isReaderOpen && !scannedCode && (
-        <Button onClick={handleButtonClick}>Mit Maschine interagieren</Button>
-      )}
-      {renderMessage()}
-      {isReaderOpen && !scannedCode && (
-        <QrReader delay={300} onError={handleError} onScan={handleScan} className="qr-reader" />
-      )}
-    </div>
-  );
+    return (
+        <div className="container">
+            {!isReaderOpen && !scannedCode && (
+                <Button onClick={handleButtonClick}>Mit Maschine interagieren</Button>
+            )}
+            {renderMessage()}
+            {isReaderOpen && !scannedCode && (
+                <QrReader delay={300} onError={handleError} onScan={handleScan} className="qr-reader" />
+            )}
+        </div>
+    );
 };
 
 export default App;
